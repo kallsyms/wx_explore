5cf415a613e1cea0c9d5d32fbcb5e8cff04fbdc1
Nick Gregory <computerfreak97@gmail.com>
2018-01-18 01:26:03 -0600

    script fixes

--- setup16_04.sh
+++ setup16_04.sh
@@ -1,26 +1,35 @@
 #!/bin/bash
 
-sudo add-apt-repository ppa:ubuntugis/ppa
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y git curl wget vim build-essential libbz2-dev libssl-dev libreadline-dev libsqlite3-dev tk-dev libpng-dev libfreetype6-dev \
-    software-properties-common \
-    gdal-bin libgdal-dev \
+sudo apt install -y git curl wget vim build-essential \
+    libbz2-dev libssl-dev libreadline-dev libsqlite3-dev tk-dev libpng-dev libfreetype6-dev \
+    software-properties-common
+
+sudo add-apt-repository -y ppa:ubuntugis/ppa
+sudo apt update
+sudo apt install -y gdal-bin libgdal-dev \
     libgrib-api-dev
 
 curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
-pyenv install 3.6.4
-pyenv virtualenv 3.6.4 wx_explore
-pyenv global wx_explore
 
-cat << EOF >> ~/.bash_profile
+cat << EOF >> ~/.bashrc
+
 export PATH="$HOME/.pyenv/bin:\$PATH"
 eval "\$(pyenv init -)"
 eval "\$(pyenv virtualenv-init -)"
 EOF
 
-source ~/.bash_profile
+export PATH="$HOME/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
+pyenv install 3.6.4
+pyenv virtualenv 3.6.4 wx_explore
+pyenv global wx_explore
 
 pip install --global-option=build_ext --global-option="-I/usr/include/gdal" GDAL==2.1.0
 pip install numpy pyproj
 pip install -r requirements.txt
+
+echo "Be sure to setup a config in wx_explore/web/config.py"
