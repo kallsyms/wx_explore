2d155ec920db30f41907579fcf41ed46574135ad
Nick Gregory <ghost@capsule8.com>
2019-06-08 21:15:36 -0400

    server side color maps

--- render_grib_field/__init__.py
+++ render_grib_field/__init__.py
@@ -3,7 +3,9 @@ from PIL import Image
 
 import azure.functions as func
 import base64
+import collections
 import io
+import matplotlib.colors
 import numpy
 import pygrib
 import tempfile
@@ -35,13 +37,54 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             )
 
         grb = pygrib.open(f.name)
-        msg = grb.read(1)[0]
+        try:
+            msg = grb.read(1)[0]
+        except OSError:
+            return func.HttpResponse(
+                "Potentially mismatched idx url",
+                status_code=400,
+            )
+
         data = msg.data()[0]
 
         # flip vertically so north is up
-        data = data[::-1]
+        data = numpy.int16(data[::-1])
 
-        img = Image.fromarray(numpy.int16(data))
+        if 'cm' in args:
+            # val0, r0, g0, b0; val1, r1, g1, b1; ...
+            ColorMapEntry = collections.namedtuple('ColorMapEntry', ['val', 'r', 'g', 'b'])
+
+            try:
+                cm_data = [ColorMapEntry(*map(float, cme.split(','))) for cme in args['cm'].split(';')]
+            except:
+                return func.HttpResponse(
+                    "Malformed color map",
+                    status_code=400,
+                )
+
+            norm = matplotlib.colors.Normalize(
+                vmin=min([cme.val for cme in cm_data]),
+                vmax=max([cme.val for cme in cm_data]),
+            )
+
+            cdict = {
+                'red': [(norm(cme.val), cme.r/255.0, cme.r/255.0) for cme in cm_data],
+                'green': [(norm(cme.val), cme.g/255.0, cme.g/255.0) for cme in cm_data],
+                'blue': [(norm(cme.val), cme.b/255.0, cme.b/255.0) for cme in cm_data],
+            }
+
+            cm = matplotlib.colors.LinearSegmentedColormap(
+                'cm',
+                cdict,
+            )
+
+            if args.get('cm_mask_under'):
+                cm.set_under(alpha=0)
+
+            img = Image.fromarray(numpy.uint8(cm(norm(data))*255), 'RGBA')
+        else:
+            img = Image.fromarray(data)
+
         img_data = io.BytesIO()
 
         img.save(img_data, format='PNG')
