29e90a32a9f1f196701515ddc71bea91d4db9962
Nick Gregory <computerfreak97@gmail.com>
2019-09-24 20:29:00 -0400

    warp based on flow

--- rta/optical_flow.py
+++ rta/optical_flow.py
@@ -19,8 +19,19 @@ frame_b = frame_b.clip(min=0).astype(np.uint8)
 #frame_a = cv2.adaptiveThreshold(frame_a, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 31, 0)
 #frame_b = cv2.adaptiveThreshold(frame_b, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 31, 0)
 
+# TODO: param tuning
 flow = cv2.calcOpticalFlowFarneback(frame_a, frame_b, None, 0.3, 3, 15, 3, 5, 1.2, 0)
 u, v = flow.transpose((2,0,1))
+
+def warp_flow(img, flow):
+    h, w = flow.shape[:2]
+    flow = -flow
+    flow[:,:,0] += np.arange(w)
+    flow[:,:,1] += np.arange(h)[:,np.newaxis]
+    res = cv2.remap(img, flow, None, cv2.INTER_LINEAR)
+    return res
+
+res = warp_flow(frame_a, flow)
 
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
@@ -36,6 +47,19 @@ qpts = np.meshgrid(sy, sx)
 m.pcolormesh(ax,ay,frame_a,shading='flat',cmap=plt.cm.jet)
 
 # larger scale = smaller arrows
-m.quiver(ax[qpts], ay[qpts], u[qpts], v[qpts])
+#m.quiver(ax[qpts], ay[qpts], u[qpts], v[qpts], scale=100)
+
+plt.show()
+
+m.pcolormesh(ax,ay,res,shading='flat',cmap=plt.cm.jet)
+
+# larger scale = smaller arrows
+#m.quiver(ax[qpts], ay[qpts], u[qpts], v[qpts], scale=100)
+
+plt.show()
+m.pcolormesh(ax,ay,frame_b,shading='flat',cmap=plt.cm.jet)
+
+# larger scale = smaller arrows
+#m.quiver(ax[qpts], ay[qpts], u[qpts], v[qpts], scale=100)
 
 plt.show()
