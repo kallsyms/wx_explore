dfb0d1204c2e3bb12df5a8163f269736d55edc1f
Nick Gregory <computerfreak97@gmail.com>
2020-03-01 23:39:24 -0500

    frontend sentry

--- ui/wx_explore/src/index.js
+++ ui/wx_explore/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/browser';
 import './index.css';
 import App from './App';
+
+Sentry.init({dsn: process.env.SENTRY_DSN});
 
 ReactDOM.render(<App />, document.getElementById('root'));
