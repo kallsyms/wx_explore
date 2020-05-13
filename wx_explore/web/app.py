d63071aa54445ac38c1a992a08c0b13f932ae92e
Nick Gregory <computerfreak97@gmail.com>
2020-05-12 23:26:06 -0400

    drop db connections before gunicorn fork

--- wx_explore/web/app.py
+++ wx_explore/web/app.py
@@ -5,7 +5,7 @@ FlaskInstrumentor().instrument()
 from wx_explore.common.logging import init_sentry
 init_sentry(flask=True)
 
-from wx_explore.web.core import app
+from wx_explore.web.core import app, db
 
 from wx_explore.common.tracing import init_tracing
 init_tracing('api')
@@ -15,3 +15,7 @@ app.register_blueprint(api)
 
 from wx_explore.common.location import preload_coordinate_lookup_meta
 preload_coordinate_lookup_meta()
+
+# Clear so gunicorn forks with no (shared) engine
+db.session.remove()
+db.engine.dispose()
