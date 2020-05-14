28be613832f49d96f4aa32744e85ca83092263b4
Nick Gregory <computerfreak97@gmail.com>
2020-05-14 16:50:40 -0400

    initialize tracing and sentry after gunicorn fork

--- wx_explore/web/app.py
+++ wx_explore/web/app.py
@@ -2,14 +2,7 @@ from opentelemetry.ext.flask import FlaskInstrumentor
 
 FlaskInstrumentor().instrument()
 
-from wx_explore.common.logging import init_sentry
-init_sentry(flask=True)
-
 from wx_explore.web.core import app, db
-
-from wx_explore.common.tracing import init_tracing
-init_tracing('api')
-
 from wx_explore.web.api import api
 app.register_blueprint(api)
 
@@ -19,3 +12,16 @@ preload_coordinate_lookup_meta()
 # Clear so gunicorn forks with no (shared) engine
 db.session.remove()
 db.engine.dispose()
+
+@app.before_first_request
+def post_fork_init():
+    """
+    Sentry and tracing are initialized late (after gunicorn fork) so that
+    any network sessions they create during init aren't shared between
+    processes.
+    """
+    from wx_explore.common.logging import init_sentry
+    from wx_explore.common.tracing import init_tracing
+
+    init_sentry(flask=True)
+    init_tracing('api')
