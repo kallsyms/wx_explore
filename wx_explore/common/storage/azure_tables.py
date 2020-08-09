5009fc426bd3bbe7fa5f6063ba299f34982233dc
Nick Gregory <computerfreak97@gmail.com>
2020-08-09 18:33:56 -0400

    shard rows in azure storage by 128 x-values

--- wx_explore/common/storage/azure_tables.py
+++ wx_explore/common/storage/azure_tables.py
@@ -10,7 +10,6 @@ from typing import Dict, Tuple, List
 import array
 import concurrent.futures
 import datetime
-import dateutil.parser
 import logging
 import numpy
 import zlib
@@ -25,10 +24,29 @@ from wx_explore.common.utils import chunk
 
 
 class AzureTableBackend(DataProvider):
+    """
+    ATS constraints:
+        * entity (row) size <= 1mb
+        * up to 255 props per row (incl. partitionkey, rowkey, timestamp)
+        * each prop <= 64k
+
+    We use the following:
+        * pk is (proj_id, y)
+        * row is (valid_time, run_time, x_shard)
+        * properties are "sf{n}" -> zlib'd bytearr value for each x in the shard
+
+    This means:
+        * Location queries are always on a single partition
+        * Valid time based filtering (less than, greater than) via lexicographical compares on row
+        * Minimal amount of data sent back since any given row in storage is only n_x_per_row dwords
+        * X sharding also means we can store ensemble results in a single property (TODO)
+    """
+
     logger: logging.Logger
     account_name: str
     account_key: str
     table_name: str
+    n_x_per_row: int = 128
 
     def __init__(self, account_name, account_key, table_name):
         logging.getLogger('azure').setLevel(logging.WARNING)
@@ -38,12 +56,6 @@ class AzureTableBackend(DataProvider):
         self.account_name = account_name
         self.account_key = account_key
         self.table_name = table_name
-
-    # entity size 1mb, up to 255 props
-    # pk is (proj_id, y)
-    # row valid,run
-    # key
-        # field -> bytearr value for each x (~4k each, max 64k)
 
     def get_fields(
             self,
@@ -56,9 +68,11 @@ class AzureTableBackend(DataProvider):
         start = start.replace(microsecond=0)
         end = end.replace(microsecond=0)
 
+        parallel_hours = 3
+
         times = [start]
-        while times[-1] + datetime.timedelta(hours=1) < end:
-            times.append(times[-1] + datetime.timedelta(hours=1))
+        while times[-1] + datetime.timedelta(hours=parallel_hours) < end:
+            times.append(times[-1] + datetime.timedelta(hours=parallel_hours))
         times.append(end)
 
         with concurrent.futures.ThreadPoolExecutor(max_workers=len(times)-1) as ex:
@@ -81,32 +95,33 @@ class AzureTableBackend(DataProvider):
         x, y = loc
         partition = f"{proj_id}-{y}"
 
+        nearest_row_x = ((x // self.n_x_per_row) * self.n_x_per_row)
+        rel_x = x - nearest_row_x
+
         # This actually (ab)uses lexicographical string compares on the rowkey
         row_start = start.isoformat()
         row_end = end.isoformat()
 
-        az_filter = f"PartitionKey eq '{partition}' and RowKey gt '{row_start}' and RowKey lt '{row_end}'"
-        select = ['PartitionKey', 'RowKey', *(f"f{sf.id}" for sf in valid_source_fields)]
+        az_filter = f"PartitionKey eq '{partition}' and RowKey gt '{row_start}' and RowKey lt '{row_end}' and XShard eq {nearest_row_x}"
+        select = ['PartitionKey', 'RowKey', 'ValidTime', 'RunTime', *(f"sf{sf.id}" for sf in valid_source_fields)]
 
         data_points = []
 
         for row in TableService(self.account_name, self.account_key).query_entities(self.table_name, az_filter, ','.join(select)):
-            valid_time, run_time = map(dateutil.parser.parse, row.RowKey.split(','))
-
             for sf in valid_source_fields:
-                key = f"f{sf.id}"
+                key = f"sf{sf.id}"
                 if key not in row or row[key] is None:
                     continue
 
                 raw = zlib.decompress(row[key].value)
-                val = array.array("f", raw).tolist()[x]
+                val = array.array("f", raw).tolist()[rel_x]
 
                 data_point = DataPointSet(
                     values=[val],
                     metric_id=sf.metric.id,
-                    valid_time=valid_time,
+                    valid_time=row.ValidTime,
                     source_field_id=sf.id,
-                    run_time=run_time,
+                    run_time=row.RunTime,
                 )
 
                 data_points.append(data_point)
@@ -124,24 +139,34 @@ class AzureTableBackend(DataProvider):
 
     def _put_fields_worker(self, proj: Projection, fields: Dict[Tuple[int, datetime.datetime, datetime.datetime], List[numpy.array]], y: int):
         partition = f"{proj.id}-{y}"
-        rows = {}
+        rows: Dict[Tuple[datetime.datetime, datetime.datetime, int], Dict[str, EntityProperty]] = {}
 
         for (field_id, valid_time, run_time), msgs in fields.items():
-            row_key = f"{valid_time.isoformat()},{run_time.isoformat()}"
-            if row_key not in rows:
-                rows[row_key] = {}
+            for x in range(0, proj.n_x, self.n_x_per_row):
+                row_key = (valid_time, run_time, x)
 
-            for msg in msgs:
-                # XXX: this only keeps last msg per field breaking ensembles
-                rows[row_key][f"f{field_id}"] = EntityProperty(EdmType.BINARY, zlib.compress(msg[y].tobytes()))
+                if row_key not in rows:
+                    rows[row_key] = {}
 
-        with TableService(self.account_name, self.account_key).batch(self.table_name) as batch:
-            for row_key, row in rows.items():
-                batch.insert_or_merge_entity({
-                    'PartitionKey': partition,
-                    'RowKey': row_key,
-                    **row,
-                })
+                for msg in msgs:
+                    # XXX: this only keeps last msg per field breaking ensembles
+                    rows[row_key][f"sf{field_id}"] = EntityProperty(EdmType.BINARY, zlib.compress(msg[y][x:x+self.n_x_per_row].tobytes()))
+
+        for row_chunk in chunk(rows.items(), 100):
+            with TableService(self.account_name, self.account_key).batch(self.table_name) as batch:
+                for row_key, row in row_chunk:
+                    valid_time, run_time, x = row_key
+                    batch.insert_entity({
+                        'PartitionKey': partition,
+                        'RowKey': f"{valid_time.isoformat()},{run_time.isoformat()},{x}",
+                        'XShard': x,
+                        # These 2 are needed for reading (reconstructing a DataPointSet).
+                        # Technically redundant (both are already in row key) but it makes reading a bit cleaner
+                        # and storage overhead is minimal.
+                        'ValidTime': valid_time,
+                        'RunTime': run_time,
+                        **row,
+                    })
 
     def clean(self, oldest_time: datetime.datetime):
         earliest = oldest_time.replace(microsecond=0)
