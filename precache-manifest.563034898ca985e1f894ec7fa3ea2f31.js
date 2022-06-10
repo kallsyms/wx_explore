1fd444bdeda520863c2cf077bcbd24ed2c6ce2f8
kallsyms <kallsyms@users.noreply.github.com>
2022-06-10 04:11:35 +0000

    deploy: 7bb3a950139e886019e6e4fd7afd1e67c83b50a0

---
+++ precache-manifest.563034898ca985e1f894ec7fa3ea2f31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cc591f9e139d269ba365c9b86056eb9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ccb3c8b33bc02e29144",
+    "url": "/static/css/main.546a2bc9.chunk.css"
+  },
+  {
+    "revision": "4b5d4dc1f50ad227af49",
+    "url": "/static/js/2.a35fa260.chunk.js"
+  },
+  {
+    "revision": "0ccb3c8b33bc02e29144",
+    "url": "/static/js/main.5d3590cc.chunk.js"
+  },
+  {
+    "revision": "a704dad5c1e357327d66",
+    "url": "/static/js/runtime-main.1eb1b677.js"
+  }
+]);
