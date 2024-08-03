dd3b8fc4476bf13d7aaea9ba608f4139713b405d
Nick Gregory <nick@nickgregory.me>
2024-08-02 23:46:13 -0400

    ditch deprecated before_first_request

--- wx_explore/web/app.py
+++ wx_explore/web/app.py
@@ -1,23 +1,6 @@
-from wx_explore.web.core import app, db
+from wx_explore.web.core import app
 from wx_explore.web.api import api
 app.register_blueprint(api)
 
 from wx_explore.common.location import preload_coordinate_lookup_meta
 preload_coordinate_lookup_meta()
-
-# Clear so gunicorn forks with no (shared) engine
-db.session.remove()
-db.engine.dispose()
-
-@app.before_first_request
-def post_fork_init():
-    """
-    Sentry and tracing are initialized late (after gunicorn fork) so that
-    any network sessions they create during init aren't shared between
-    processes.
-    """
-    from wx_explore.common.logging import init_sentry
-    from wx_explore.common.tracing import init_tracing
-
-    init_sentry(flask=True)
-    init_tracing('api')
